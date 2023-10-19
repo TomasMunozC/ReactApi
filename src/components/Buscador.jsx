c8848c91f75a8498e2767c21b8892ab5f01d23ef
TomasMunozC <tomasmunozc@gmail.com>
2023-10-19 20:06:30 -0300

    first commit

---
+++ src/components/Buscador.jsx
@@ -0,0 +1,18 @@
+const Buscador = ({ filtrar }) => {
+    const handleBuscar = (terminoBusqueda) => {
+        const terminoBusquedaLowerCase = terminoBusqueda.toLowerCase();
+        filtrar(terminoBusquedaLowerCase);
+    };
+    return (
+        <div className="input">
+            <input
+                className="form-control mt-3"
+                type="text"
+                placeholder="Buscador sismologico"
+                onChange={(e) => handleBuscar(e.target.value)}
+            />
+        </div>
+    );
+};
+
+export default Buscador;
